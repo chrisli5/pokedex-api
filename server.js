655d52dadc598b659a58e518de6d719db59d1518
chrisli5 <chrisli41@gmail.com>
2019-08-24 20:59:35 -0700

    fix logging

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const POKEDEX = require('./pokedex.json')
 const app = express()
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morganSetting)
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors());
 
