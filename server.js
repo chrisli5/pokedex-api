1228b82865918a787ae9b35c52929f4f2b2e8188
chrisli5 <chrisli41@gmail.com>
2019-08-24 20:53:02 -0700

    prepare for deploy

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const POKEDEX = require('./pokedex.json')
 
 const app = express()
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan('dev'))
+app.use(morganSetting)
+app.use(helmet())
+app.use(cors());
 
-app.use((req, res) => {
-  res.send('Hello, world!')
-})
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  debugger
+  next();
+});
 
-const PORT = 8000
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+function handleGetTypes(req, res) {
+  res.status(200).json(validTypes)
+}
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  const { name, type } = req.query;
+  let response = POKEDEX.pokemon;
+
+  if (name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(name.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(type)
+    )
+  }
+
+  res.json(response)
+
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
